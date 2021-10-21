ca4553e35688d97f027b2319c686f4c80b31e8ff
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-10-21 17:10:07 -0400

    Trying to get server connect

--- includes/functions.php
+++ includes/functions.php
@@ -1,23 +1,14 @@
 <?php
-    // storeresults in a variable
+    include("connect.php");
+    
+    $query = "SELECT * FROM tbl_design, tbl_development";
+
+    $runQuery = $pdo->query($query);
+
     $result = array();
 
-    // if a user passes an ID via a query string (?id=1)
-    // then we should retrieve the row of data that matches and pass it back to the app
-    function getPortfolioData($conn, $tbl_design) {
-        if (is_null($tbl_design)){
-            $query = "SELECT * FROM tbl_design";
-        } else{
-                $query = "SELECT * FROM tbl_design WHERE id='".$tbl_design."'";
-            }
-                    
-        // this is the database result -> the raw data that SQL gives us
-        $runQuery = $conn->query($query);    
-
-        // process our DB result and make something we can use with AJAX
-        while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
-         $result[] = $row;
-        }
-
-        return $result;
+    while($row = $runQuery-> fetchALL (PDO::FETCH_ASSOC)){
+        $result[] = $row;
     }
+
+    echo (json_encode($result));
