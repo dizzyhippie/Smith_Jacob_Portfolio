973a9602953e0eac248624ce4a26cf1d9db5a214
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-10-21 13:41:59 -0400

    Adding PHP shells

---
+++ includes/index.php
@@ -0,0 +1,14 @@
+<?php
+    require("connect.php");
+    require("functions.php");
+
+   // filter the incoming basic request and call the appropriate function
+
+   if (isset($_GET["id"])) {
+       $id = $_GET["id"];
+       $result = getOneProf($pdo, $id);
+   } else {
+       $result = getAllProfs($pdo);
+   }
+
+   echo json_encode($result);
