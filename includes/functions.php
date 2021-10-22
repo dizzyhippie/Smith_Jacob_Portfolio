4b2efa11eec0091e0c5bd6c90caca1074002b47a
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-10-22 12:55:19 -0400

    Getting response tested in Postman

--- includes/functions.php
+++ includes/functions.php
@@ -1,14 +1,23 @@
 <?php
-    include("connect.php");
-    
-    $query = "SELECT * FROM tbl_projects";
-
-    $runQuery = $pdo->query($query);
-
+    // store the processed results in a variable
     $result = array();
 
-    while($row = $runQuery-> fetchALL (PDO::FETCH_ASSOC)){
-        $result[] = $row;
-    }
+    // if a user passes an ID via a query string (?id=1)
+    // then we should retrieve the row of data that matches and pass it back to the app
+    function getPortfolioData($conn, $project) {
+        if (is_null($project)){
+            $query = "SELECT * FROM tbl_projects";
+        } else{
+                $query = "SELECT * FROM tbl_projects WHERE id='".$project."'";
+            }
+                    
+        // this is the database result -> the raw data that SQL gives us
+        $runQuery = $conn->query($query);    
 
-    echo (json_encode($result));
+        // process our DB result and make something we can use with AJAX
+        while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+         $result[] = $row;
+        }
+
+        return $result;
+    }
--- js/components/dataMiner.js
+++ js/components/dataMiner.js
@@ -1,13 +1,25 @@
-let projects = {};
+function getPortfolioData(params, cb) {
+    //Fetch API will retrieve data
+    let baseURL = "./includes/index.php";
 
-function getData(){
-    fetch ("./includes/functions.php")
-    .then (res => res.json())
+    if (params) {
+        let keys = Object.keys(params); // takes an object and creates an array of the keys ["id", "name"]
+
+        // look at the MDN reference for Array.map to see what it does
+        // it just re-uses the values however you like -> in this case we're building some text
+        let newQStringParams = keys.map(key => `${key}=${params[key]}`).join("&&");
+
+        baseURL += `?${newQStringParams}`;
+    }    
+    
+    fetch(baseURL)
+    .then(res => res.json()) // unpack the API response (turn it back into plain JS)
     .then(data => {
-        console.table(data);
-        projects = data;
+        
+        // call a function to generate our dynamic content Below here
+        
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error(error)); // catch and report any errors
 }
 
-export { getData }
+export { getPortfolioData }
