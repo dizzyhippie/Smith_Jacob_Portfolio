b149f438d9df8dbeec94fa88651d929e06b97c47
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-10-22 16:18:44 -0400

    Modal is functional
    modal functioning and can be activated by clicking on the portfolio piece.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 import { getPortfolioData } from "./components/dataMiner.js";
 (() => {
-    const   portfolio = document.querySelector("#portfolio"),
-            theTemplate = document.querySelector("#portfolio-template").content;
+const   portfolio = document.querySelector("#portfolio"),
+        theTemplate = document.querySelector("#portfolio-template").content,
+        closeButton = document.querySelector(".close"),
+        buttons = document.querySelectorAll(".open");
 
-            function buildTeam(info) {
-                info.forEach(project=> {
-                    let panel = theTemplate.cloneNode(true),
-                        projectInfo = panel.firstElementChild.children;
+function buildTeam(info) {
+    info.forEach(project => {
+        let panel = theTemplate.cloneNode(true),
+        projectInfo = panel.firstElementChild.children;
         
-                    // add the custom data attribute value to the section tag inside the template
-                    panel.firstElementChild.dataset.key = project.id;
+        //custom data attribute value to the section tag inside the template
+        panel.firstElementChild.dataset.key = project.id;
 
-                    projectInfo[0].textContent = project.name;
-                    projectInfo[1].textContent = project.industry;
-                    
+        projectInfo[0].textContent = project.name;
+        projectInfo[1].textContent = project.industry;
         
-                    // put the virtual panel in the team section in our HTML page
-                    portfolio.appendChild(panel);
-                })
-            }
+        // put the virtual panel in the team section in our HTML page
+        portfolio.appendChild(panel);
+    })
+}
 
-            getPortfolioData(null, buildTeam);
+function openModal(){
+    
+    let modal = document.querySelector(".modal-panel");
+    modal.style.display = "block";
 
-   
+    //let key = this.dataset.key;
+
+    closeButton.addEventListener('click', () => {
+        modal.style.display = "none";
+      }) 
+}
+
+getPortfolioData(null, buildTeam);
+
+portfolio.addEventListener("click", openModal)
 })()
