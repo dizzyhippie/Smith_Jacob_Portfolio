c1fbb063a79dd7d771a3a2b5676bbd2030bd3fc1
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-10-27 19:36:12 -0400

    Vue Setup - not functioning

---
+++ js/components/TheGalleryThumbnail.js
@@ -0,0 +1,8 @@
+export default {
+    name: "TheGalleryThumbnail",
+    templates: `
+    <div class="portfolio-pieces" data-key="">
+        <h2 class="p_name"></h2>
+        <h3 class="p_industry"></h3>
+    </div>`
+}
