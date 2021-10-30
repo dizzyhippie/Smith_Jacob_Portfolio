448adba93c6b7a6448b16da6d7ce9e038cd28c3f
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-10-29 22:34:56 -0400

    Modal and Tiles Functional
    Modal needs to load proper info

--- js/components/TheGalleryThumbnail.js
+++ js/components/TheGalleryThumbnail.js
@@ -1,8 +1,22 @@
 export default {
     name: "TheGalleryThumbnail",
-    templates: `
-    <div class="portfolio-pieces" data-key="">
-        <h2 class="p_name"></h2>
-        <h3 class="p_industry"></h3>
+
+    props: ["piece"],
+
+    methods: {
+        openModal(){
+            console.log("You selected");
+            let modal = document.querySelector(".modal-panel");
+            let closeButton = document.querySelector(".close")
+            modal.style.display = "block";
+            closeButton.addEventListener('click', () => { modal.style.display = "none";})
+        }
+    },
+
+    template: `
+    <div class="portfolio-pieces" data-key="" @click="openModal">
+        <h2 class="p_name">{{ piece.name }}</h2>
+        <h3 class="p_industry"> {{ piece.industry }} </h3>
+        <button
     </div>`
 }
