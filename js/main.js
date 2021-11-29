7f588d5a3436bdd93cbc0e8c911efc0ef7944a69
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-11-29 12:32:48 -0500

    About Module working nice

--- js/main.js
+++ js/main.js
@@ -6,11 +6,12 @@ import TheModal from "./components/TheModal.js";
     const myVue = new Vue ({
         created: function() {
             //fetch portfolio data
-            getPortfolioData(null, (data) => this.portfolioData = data)
+            getPortfolioData(null, (data) => this.portfolioData = data );
         },
 
         data: {
             portfolioData: [],
+            aboutData: [],
             currentItem : {}
         },
 
@@ -22,7 +23,7 @@ import TheModal from "./components/TheModal.js";
 
         components: {
             tile: TheGalleryThumbnail,
-            modal: TheModal
+            modal: TheModal,
         }
     
     }).$mount("#app");
